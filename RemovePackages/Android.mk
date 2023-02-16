6080225b1109c1f232d2053ff07d30566a0094b6
Asif <najmulhasan3609@gmail.com>
2023-02-16 17:33:47 +0000

    jasmine_sprout: debloat extra gapps

---
+++ RemovePackages/Android.mk
@@ -0,0 +1,40 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := RemovePackages
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_TAGS := optional
+LOCAL_OVERRIDES_PACKAGES := \
+    Chrome \
+    Chrome-Stub \
+    Drive \
+    GoogleCamera \
+    Maps \
+    MyVerizonServices \
+    OBDM_Permissions \
+    GoogleTTS \
+    Velvet \
+    MaestroPrebuilt \
+    AndroidAutoStubPrebuilt \
+    talkback \
+    SafetyHubPrebuilt \
+    SoundAmplifierPrebuilt \
+    obdm_stub \
+    OemDmTrigger \
+    OPScreenRecord \
+    Ornament \
+    SafetyHubPrebuilt \
+    ScribePrebuilt \
+    Showcase \
+    Snap \
+    MusicFX \
+    SprintDM \
+    SprintHM \
+    VZWAPNLib \
+    VzwOmaTrigger \
+    YouTube \
+    YouTubeMusicPrebuilt
+LOCAL_UNINSTALLABLE_MODULE := true
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_SRC_FILES := /dev/null
+include $(BUILD_PREBUILT)
