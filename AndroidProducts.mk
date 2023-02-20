8790e62cad2e9806cd8830b37d793213537d7e9a
Asif <najmulhasan3609@gmail.com>
2023-02-20 09:38:52 +0000

    jasmine_sprout: Evox bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,9 +6,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_jasmine_sprout.mk
+    $(LOCAL_DIR)/evolution_jasmine_sprout.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_jasmine_sprout-user \
-    aosp_jasmine_sprout-userdebug \
-    aosp_jasmine_sprout-eng
+    evolution_jasmine_sprout-user \
+    evolution_jasmine_sprout-userdebug \
+    evolution_jasmine_sprout-eng
--- evolution_jasmine_sprout.mk
+++ evolution_jasmine_sprout.mk
@@ -10,7 +10,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Pixel Experience stuff
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 TARGET_INCLUDE_STOCK_ARCORE := false
 
 # We are a phone
@@ -29,6 +29,8 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := jasmine_sprout
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := Mi A2
-PRODUCT_NAME := aosp_jasmine_sprout
+PRODUCT_NAME := evolution_jasmine_sprout
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+TARGET_USES_MINI_GAPPS := true
+EVO_BUILD_TYPE := Custom
